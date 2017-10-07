f3644542410b17b6e3ed92d5a86a33fca69dd37f
Michael Steidl <mwsteidl@newsit.biz>
2017-10-07 15:26:50 +0200

    Sample Turtle files imported from http://odrlapi.appspot.com/

--- main.js
+++ main.js
@@ -9,11 +9,19 @@ function transformTurtleToJsonld(samplename) {
         return
     }
 
+    let turtleFilename = './testdata/' + samplename + '.ttl'
+    if (!fs.existsSync(turtleFilename)){
+        console.log("Turtle file not found: " + turtleFilename)
+        return;
+    }
+    console.log('Turtle file will be transformed: ' + turtleFilename)
+
     let turtledata
     try {
-        turtledata = fs.readFileSync('./testdata/' + samplename + '.ttl', 'utf8');
+        turtledata = fs.readFileSync(turtleFilename, 'utf8');
     } catch(e) {
         console.log('Error:', e.stack);
+        return
     }
     // console.log(turtledata);
     let n3parser = N3.Parser()
@@ -32,6 +40,7 @@ function transformTurtleToJsonld(samplename) {
                 let context = jsonfile.readFileSync('./ODRL22.jsonld') //
                 jsonld.compact(doc, context, {skipExpansion : true},  function(err, compacted) {
                     // console.log(JSON.stringify(compacted, null, 2));
+                    compacted['@context'] = "http://www.w3.org/ns/odrl.jsonld" // use the link of the ODRL @context resource
                     jsonfile.writeFileSync('./testdata/' + samplename + '=compact.json', compacted, {spaces: 2})
                 });
             }
@@ -39,11 +48,36 @@ function transformTurtleToJsonld(samplename) {
                 console.log('Sample ' + samplename + ' not transformed, no jsonld output' )
             }
         })
-
     })
 
 }
 
-transformTurtleToJsonld('sample069x')
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
 
-transformTurtleToJsonld('sample069x-corr')
+function transformAllOdrlSamples() {
+    for(let i = 1; i < 78; i++){
+        switch(i){
+            case 32:
+            case 70:
+            case 71:
+            case 75:
+            case 76:
+            case 77:
+                continue
+                break
+        }
+        let sampleno = pad(i, 3)
+        let samplename = 'sample' + sampleno
+        transformTurtleToJsonld(samplename)
+    }
+}
+
+// transformTurtleToJsonld('sample069x')
+
+transformAllOdrlSamples()
+
+
